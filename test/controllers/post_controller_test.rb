bcf8c03ab8ffca7df669ce3d3092036f1367e513
tanacchi <q111026d@mail.kyutech.jp>
2018-06-12 15:50:32 +0900

    Add test for post#ideal

--- test/controllers/post_controller_test.rb
+++ test/controllers/post_controller_test.rb
@@ -5,4 +5,14 @@ class PostControllerTest < ActionDispatch::IntegrationTest
     get post_index_url
     assert_response :success
   end
+
+  test "should get ideal page" do
+    get post_ideal_url
+    assert_response :success
+  end
+
+  test "should get trouble page" do
+    get post_trouble_url
+    assert_response :success
+  end
 end
