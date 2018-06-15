8fe56adbec7b1573e927fe5f344054cb9e154a97
tanacchi <q111026d@mail.kyutech.jp>
2018-06-15 11:15:37 +0900

    Update controller for ideas list

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,5 +1,7 @@
 class PostController < ApplicationController
   def index
+    @posts = Genre.all
+    @ideas = Idea.all
   end
 
   def ideal
@@ -8,9 +10,13 @@ class PostController < ApplicationController
   end
 
   def trouble
+    current_genre = Genre.find_by(name: params[:action])
+	@posts = current_genre.ideas
   end
   
   def other
+    current_genre = Genre.find_by(name: params[:action])
+	@posts = current_genre.ideas
   end
   
 end
