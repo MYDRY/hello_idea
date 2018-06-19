76a399c5431c9368815312357fce01997d920af6
tanacchi <q111026d@mail.kyutech.jp>
2018-06-19 17:14:45 +0900

    Fix missing conflicts

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,22 +1,22 @@
 class PostController < ApplicationController
   def index
     @posts = Genre.all
-    @ideas = Idea.all
+    @topics = Topic.all
   end
 
   def ideal
 	current_genre = Genre.find_by(name: params[:action])
-	@posts = current_genre.ideas
+	@topics = current_genre.topics
   end
 
   def trouble
     current_genre = Genre.find_by(name: params[:action])
-	@posts = current_genre.ideas
+	@topics = current_genre.ideas
   end
   
   def other
     current_genre = Genre.find_by(name: params[:action])
-	@posts = current_genre.ideas
+	@topics = current_genre.ideas
   end
 
   def show
