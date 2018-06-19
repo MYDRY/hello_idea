5f10b7e0ba416706ca6aaefe0c9d5565f3f219b6
tanacchi <q111026d@mail.kyutech.jp>
2018-06-19 16:22:07 +0900

    Add relations around comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  belongs_to :commentable, polymorphic: true
+  has_many :comments, as: :commentable
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,4 @@
 class Idea < ApplicationRecord
   belongs_to :topic
+  has_many :comments, as: :commentable 
 end
