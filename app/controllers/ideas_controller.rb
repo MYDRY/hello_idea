571e0266be00425a350d09853ef0d63a50a98fba
rkdora <rryuto5210@gmail.com>
2018-06-23 13:43:03 +0900

    Add new_idea_page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,4 +2,20 @@ class IdeasController < ApplicationController
   def show
     @idea = Idea.find(params[:id])
   end
+
+def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(topic_id: params[:id])
+    @idea.save
+    redirect_to topic_path
+  end
+
+
+  private
+  def idea_params
+    params.require(:idea).permit(:title, :body, :topic_id)
+  end
 end
