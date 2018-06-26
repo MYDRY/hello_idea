34380ae3816c722495f2667c8ab0badb6098995f
tanacchi <q111026d@mail.kyutech.jp>
2018-06-26 17:38:20 +0900

    Add action about login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#top'
   post 'topics/classfy_topic'
   get '/login' => 'users#login'
+  post '/login' => 'users#check_user'
   resources :users
   resources :topics do
     resources :ideas do
