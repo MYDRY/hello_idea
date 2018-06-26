97f6b873947413f0a0eee5688fc5f1f01ca1cb8e
tanacchi <q111026d@mail.kyutech.jp>
2018-06-26 16:42:34 +0900

    Add validates for presence of users name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'example', email: 'example@com')
+  end
+
+  test "name should be present" do
+    @user.name = '  ';
+    assert_not @user.valid?
+  end
 end
