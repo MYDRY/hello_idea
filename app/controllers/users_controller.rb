553359bcbe0a00c077fbefe77a720f4156819ecd
tanacchi <q111026d@mail.kyutech.jp>
2018-06-26 16:33:00 +0900

    Complete new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to @user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
