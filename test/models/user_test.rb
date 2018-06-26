8dc1841bd0fc37e91ca9aef92e3a07e0029c4e6a
tanacchi <q111026d@mail.kyutech.jp>
2018-06-26 17:08:52 +0900

    Add validate around users name and email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,10 +3,26 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: 'example', email: 'example@com')
+    @user.save
   end
 
   test "name should be present" do
-    @user.name = '  ';
+    @user.name = '  '
     assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = '   '
+    assert_not @user.valid?
+  end
+
+  test "email should be unique" do
+    @user1 = User.new(name: 'example2', email: 'example@com')
+    assert_not @user1.valid?
+  end
+
+  test "name should be unique" do
+    @user1 = User.new(name: 'example', email: 'example2@com')
+    assert_not @user1.valid?
   end
 end
