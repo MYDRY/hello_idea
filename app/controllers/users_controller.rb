566c4fc48c406ca74367ac556b7419f12e66fc4e
tanacchi <q111026d@mail.kyutech.jp>
2018-06-26 16:25:21 +0900

    Add users#new action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,15 @@ class UsersController < ApplicationController
   def index
 	@users=User.all
   end
+  
   def show
     @user = User.find(params[:id])
-  end 
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
 end
