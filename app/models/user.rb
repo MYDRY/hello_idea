e3ad59711c10030ba14c04e22ac320e84b409e5b
tanacchi <q111026d@mail.kyutech.jp>
2018-06-26 17:08:52 +0900

    Add validate around users name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, {presence: true, uniqueness: true}
+  validates :email, {presence: true, uniqueness: true} 
 end
