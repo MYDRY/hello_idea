e8bbd772c68fb0b15c8977d891614c572f27385c
tanacchi <q111026d@mail.kyutech.jp>
2018-06-28 22:36:56 +0900

    Add test for users create action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest 
   test "users show page should exist" do
     @user = users(:user0)
     get user_url(@user)
     assert_response :success
+  end
+
+  test "users create action should create new User" do
+    assert_difference('User.count') do
+      post users_path(user: { name: 'create_test', email: 'create_test@mail.com' })
+    end
+    assert_response :redirect
+    assert_redirected_to user_path(assigns(:user))
   end
 end
