8ce34020e0f0348e0ff11bc3402995fee7cc1d0a
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 16:55:17 +0900

    Update model test with `belongs_to` association

--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -4,4 +4,9 @@ class IdeaTest < ActiveSupport::TestCase
   def setup
     @idea = ideas(:idea0)
   end
+
+  test "idea should belong to a topic" do
+    @idea.topic_id = nil
+    assert_not @idea.valid?
+  end
 end
