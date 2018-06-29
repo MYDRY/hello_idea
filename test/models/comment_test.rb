73aea82c7f05c8625237fdcfcd73f874b914055f
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 16:55:17 +0900

    Update model test with `belongs_to` association

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   def setup
-    @comment = comments(:comment0)
+    @comment_to_idea = comments(:comment_to_idea0)
+    @comment_to_comment = comments(:comment_to_comment0)
+  end
+
+  test "comment should belong to a commentable" do
+    @comment_to_idea.commentable_id = nil
+    assert_not @comment_to_idea.valid?
   end
 end
--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -4,4 +4,9 @@ class TopicTest < ActiveSupport::TestCase
   def setup
     @topic = topics(:topic0)
   end
+
+  test "topic should belong to a genre" do
+    @topic.genre_id = nil
+    assert_not @topic.valid?
+  end
 end
