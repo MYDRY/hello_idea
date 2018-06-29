fa5636803402bd1570ae198321106104574f709f
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 13:00:39 +0900

    Fix bug around ideas counter in ideas controller test

--- test/controllers/ideas_controller_test.rb
+++ test/controllers/ideas_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class IdeasControllerTest < ActionDispatch::IntegrationTest
   test "ideas create action should create new idea" do
     @topic = topics(:topic0)
-    assert_difference(`Idea.count`) do
+    assert_difference('Idea.count') do
       post topic_ideas_path(topic_id: @topic, idea: { title: 'test title', body: 'test_body' })
     end
     assert_response :redirect
