804d028864d19a80e7dff8f837778145022d5a43
tanacchi <q111026d@mail.kyutech.jp>
2018-07-02 18:31:13 +0900

    Add user_id column on topic and idea

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_26_061912) do
+ActiveRecord::Schema.define(version: 2018_07_02_092008) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -33,7 +33,9 @@ ActiveRecord::Schema.define(version: 2018_06_26_061912) do
     t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["topic_id"], name: "index_ideas_on_topic_id"
+    t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
@@ -42,7 +44,9 @@ ActiveRecord::Schema.define(version: 2018_06_26_061912) do
     t.integer "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["genre_id"], name: "index_topics_on_genre_id"
+    t.index ["user_id"], name: "index_topics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
