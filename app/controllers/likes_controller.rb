8512b79bf865bf26faa6a1bcf5a464a95ecfa248
tanacchi <q111026d@mail.kyutech.jp>
2018-07-03 01:58:49 +0900

    Use local valiable instead of params for argument of path

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,14 @@ class LikesController < ApplicationController
   
   def create
     @idea = Idea.find(params[:idea_id])
-    like = current_user.likes.build(idea_id: params[:idea_id])
+    like = current_user.likes.build(idea_id: @idea.id)
     like.save
     render 'ideas/show'
   end
 
   def destroy
     @idea = Idea.find(params[:id])
-    like = Like.find_by(idea_id: params[:id], user_id: current_user.id)
+    like = Like.find_by(idea_id: @idea.id, user_id: current_user.id)
     like.destroy
     render 'ideas/show'
   end
