d5dc9ed9502569d8b3b6f817af9695794ce9d4fc
tanacchi <q111026d@mail.kyutech.jp>
2018-07-02 18:32:44 +0900

    Modify model for association around user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   validates :name, {presence: true, uniqueness: true}
-  validates :email, {presence: true, uniqueness: true} 
+  validates :email, {presence: true, uniqueness: true}
+
+  has_many :ideas
+  has_many :topics
 end
