169bc2acd5a7e2bdda8290e25b7c2e87cbfb898e
tanacchi <q111026d@mail.kyutech.jp>
2018-07-02 22:44:26 +0900

    Add likes create action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,11 @@
 class LikesController < ApplicationController
+
+  before_action :authorize
+  
   def create
+    like = current_user.likes.build(idea_id: params[:idea_id])
+    like.save
+    redirect_back fallback_location: root_path
   end
 
   def destroy
