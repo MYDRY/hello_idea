905d78213e81990a29ccb71c4b7712bd8aa55f2b
tanacchi <q111026d@mail.kyutech.jp>
2018-07-03 01:51:37 +0900

    Use render instead of redirect for like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,16 @@ class LikesController < ApplicationController
   before_action :authorize
   
   def create
+    @idea = Idea.find(params[:idea_id])
     like = current_user.likes.build(idea_id: params[:idea_id])
     like.save
-    redirect_back fallback_location: root_path
+    render 'ideas/show'
   end
 
   def destroy
+    @idea = Idea.find(params[:id])
     like = Like.find_by(idea_id: params[:id], user_id: current_user.id)
     like.destroy
-    redirect_back fallback_location: root_path    
+    render 'ideas/show'
   end
 end
