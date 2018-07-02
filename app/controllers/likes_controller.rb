972d069e138074b0b113810eac64116ef8a95880
tanacchi <q111026d@mail.kyutech.jp>
2018-07-03 01:16:47 +0900

    Fix params on likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,7 +9,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    like = Like.find_by(idea_id: params[:idea_id], user_id: current_user.id)
+    like = Like.find_by(idea_id: params[:id], user_id: current_user.id)
     like.destroy
     redirect_back fallback_location: root_path    
   end
