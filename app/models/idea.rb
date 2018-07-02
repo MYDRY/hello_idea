e200671db4b21eab78c48f90052db38957b226f2
tanacchi <q111026d@mail.kyutech.jp>
2018-07-02 22:27:16 +0900

    Add user and topic model config about association

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,7 @@
 class Idea < ApplicationRecord
   belongs_to :topic
   belongs_to :user
-  has_many :comments, as: :commentable 
+
+  has_many :comments, as: :commentable
+  has_many :likes, dependent: :destroy
 end
