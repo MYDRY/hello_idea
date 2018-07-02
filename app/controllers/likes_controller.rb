694e80f17441f36815d2784a02b428cf37cee60c
tanacchi <q111026d@mail.kyutech.jp>
2018-07-02 23:02:24 +0900

    Add likes destroy action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,5 +9,8 @@ class LikesController < ApplicationController
   end
 
   def destroy
+    like = Like.find_by(idea_id: params[:idea_id], user_id: current_user.id)
+    like.destroy
+    redirect_back fallback_location: root_path    
   end
 end
