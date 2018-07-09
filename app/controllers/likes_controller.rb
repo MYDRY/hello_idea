5d58bb064a2da95d0ee9f4140ed774be3f4547d3
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-07-09 18:31:04 +0900

    Change render to redirect likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,13 @@ class LikesController < ApplicationController
     @idea = Idea.find(params[:idea_id])
     like = current_user.likes.build(idea_id: @idea.id)
     like.save
-    render 'ideas/show'
+    redirect_to idea_path(@idea.id)
   end
 
   def destroy
     @idea = Idea.find(params[:id])
     like = Like.find_by(idea_id: @idea.id, user_id: current_user.id)
     like.destroy
-    render 'ideas/show'
+    redirect_to idea_path(@idea.id)
   end
 end
