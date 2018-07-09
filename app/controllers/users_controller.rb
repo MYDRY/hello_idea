fee12bac78211b8416dd648eafb0090ecc8440ea
tanacchi <q111026d@mail.kyutech.jp>
2018-07-09 17:22:29 +0900

    Add update action on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,9 +48,18 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
   
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :image)
   end
 end
