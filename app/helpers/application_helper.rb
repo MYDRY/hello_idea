e69fcb3a7458aea5d96cec6d32b9c711f44458a2
Tanaka Daiki <q111026d@mail.kyutech.jp>
2018-07-09 19:05:28 +0900

    Revert "Develop/upgrade home signup"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,2 @@
-# coding: utf-8
 module ApplicationHelper
-  def format_datetime(datetime, type = :datetime)
-    return '' unless datetime
-
-    case type
-    when :datetime
-      format = '%Y年%m月%d日(%a) %H:%M:%S'
-    when :date
-      format = '%Y年%m月%d日(%a)'
-    when
-      format = '%H:%M:%S'
-    end
-    
-    datetime.strftime(format)
-  end
 end
