55ee71d39d2b6c25e70db0bb21ce57b8b31cd656
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-07-10 00:11:32 +0900

    Add has_secure_password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,7 +61,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:name, :email, :image)
+    params.require(:user).permit(:name, :email, :image, :password)
   end
 
   def correct_user
