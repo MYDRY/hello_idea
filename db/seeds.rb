185407e0168b1648e67afc23c71ce78a296f866e
tanacchi <q111026d@mail.kyutech.jp>
2018-07-11 14:58:54 +0900

    Use Japanese on seeds of genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,7 +6,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-initial_genres = Genre.create([{ id: 1, name: 'ideal' },
-                               { id: 2, name: 'trouble' },
-                               { id: 3, name: 'other'}
+initial_genres = Genre.create([{ id: 1, name: '理想' },
+                               { id: 2, name: '問題' },
+                               { id: 3, name: 'その他'}
                               ])
