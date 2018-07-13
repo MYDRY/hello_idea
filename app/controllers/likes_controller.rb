96b801e80d3e461ee16ad1ec1ea69979e85fa7dd
rkdora <rryuto5210@gmail.com>
2018-07-13 16:48:23 +0900

    Add flash message on likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,15 @@ class LikesController < ApplicationController
     @idea = Idea.find(params[:idea_id])
     like = current_user.likes.build(idea_id: @idea.id)
     like.save
+    flash[:success] = "いいねしました"
     redirect_to idea_path(@idea.id)
   end
 
   def destroy
     @idea = Idea.find(params[:id])
     like = Like.find_by(idea_id: @idea.id, user_id: current_user.id)
-    like.destroy
+    like.destroy 
+    flash[:success] = "いいねを取り消しました"
     redirect_to idea_path(@idea.id)
   end
 end
