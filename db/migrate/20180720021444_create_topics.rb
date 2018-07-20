7b88a7338ea5fb1d57943189b6b03f8ae760e9e9
tanacchi <q111026d@mail.kyutech.jp>
2018-07-20 11:16:59 +0900

    Add migration of topic

---
+++ db/migrate/20180720021444_create_topics.rb
@@ -0,0 +1,12 @@
+class CreateTopics < ActiveRecord::Migration[5.2]
+  def change
+    create_table :topics do |t|
+      t.string :title
+      t.text :body
+      t.references :genre, foreign_key: true
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
