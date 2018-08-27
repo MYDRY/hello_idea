46e5bad6a83d38f726ed84af62ea5bb589c2538c
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-08-27 23:06:29 +0900

    Add admin user & complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_022550) do
+ActiveRecord::Schema.define(version: 2018_08_27_133848) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2018_07_20_022550) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
