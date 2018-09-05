e6a14ef4127a805308063e4b2ee9ac275832c88f
rkdora <rryuto5210@gmail.com>
2018-09-06 01:27:41 +0900

    Fix miss about admin_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 class UsersController < ApplicationController
   before_action :authorize, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy]
+  before_action :admin_user, only: :destroy
   
   def index
   	@users=User.all
@@ -82,7 +82,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def admmin_user
+  def admin_user
     redirect _to root_path unless current_user.admin?
   end
 
