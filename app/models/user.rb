849831cc858639bf5c19e41f8333e40dd0f007e8
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-09-08 16:13:27 +0900

    Hidden email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, {presence: true, uniqueness: true}
-  validates :email, {presence: true, uniqueness: true}
+  #validates :email, {presence: true, uniqueness: true}
 
   has_many :ideas,  dependent: :destroy
   has_many :topics, dependent: :destroy
