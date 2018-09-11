e839afa5e73dd289bb5bb5e1698eabbb61e1d56c
tanacchi <q111026d@mail.kyutech.jp>
2018-09-11 17:13:59 +0900

    Add password confirmation field on signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
+# coding: utf-8
 class User < ApplicationRecord
 
   has_secure_password
 
   validates :name, {presence: true, uniqueness: true}
   #validates :email, {presence: true, uniqueness: true}
-
+  validates :password,
+            confirmation: { message: 'パスワードを再入力してください'},
+            presence: true
+  validates :password_confirmation, presence: { message: 'パスワードを再入力してください'}
+  
   has_many :ideas,  dependent: :destroy
   has_many :topics, dependent: :destroy
   has_many :likes,  dependent: :destroy
