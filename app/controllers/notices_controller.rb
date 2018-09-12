134cc1e55edeaca65ad2f84467defa97ba67e1a1
tanacchi <q111026d@mail.kyutech.jp>
2018-09-12 18:02:18 +0900

    Add mark action to notices_controller

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -2,4 +2,11 @@ class NoticesController < ApplicationController
   def index
     @notices = current_user.notices
   end
+
+  def mark
+    @notice = Notice.find(params[:id])
+    @notice.mark_as_read
+    @notice.save
+    redirect_to  user_notices_path(user_id: current_user)
+  end
 end
