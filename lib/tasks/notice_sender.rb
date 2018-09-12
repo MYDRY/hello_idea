92352849f657316d33d10733f1e6c34a0d07aa4e
tanacchi <q111026d@mail.kyutech.jp>
2018-09-13 02:59:30 +0900

    Update notice sender script

--- lib/tasks/notice_sender.rb
+++ lib/tasks/notice_sender.rb
@@ -1,5 +1,16 @@
+file_name = ARGV[0]
+message_string = ''
+
+begin
+  file = open(file_name)
+  message_string = file.read
+rescue => err
+  puts '[ERROR] Failed to read message file.'
+  puts err.message
+end
+
 User.transaction do
   User.all.each do |user|
-    user.notices.create(message: 'Notice from developers.')
+    user.notices.create(message: message_string)
   end
 end
