7bc41dbf776c67d29b849012c11f8f1ed1cd9e39
tanacchi <q111026d@mail.kyutech.jp>
2018-09-12 20:08:13 +0900

    Add destroy_has_read action on notices_controller

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -15,4 +15,12 @@ class NoticesController < ApplicationController
     @notice.destroy
     redirect_to  user_notices_path(user_id: current_user)
   end
+
+  def destroy_has_read
+    @notices = current_user.notices.where(has_read: true)
+    unless @notices.empty?
+      @notices.destroy_all
+    end
+    redirect_to  user_notices_path(user_id: current_user)
+  end
 end
