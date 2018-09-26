16d293b9d7908b5d3cb68d5ed5faf35cea21117e
tanacchi <q111026d@mail.kyutech.jp>
2018-09-26 20:21:36 +0900

    [WIP] Use helper to show mandalart elem
    全く同じコードが生成されているにも関わらず表示が崩れている

--- app/helpers/mandalarts_helper.rb
+++ app/helpers/mandalarts_helper.rb
@@ -1,2 +1,14 @@
 module MandalartsHelper
+  def put_sub_elem
+    html_text = ''
+    (1..9).each do |n|
+      if n == 5 
+        elem = '<input id="1-' +  n.to_s + '" type="text" class="col-xl-3" style="visibility: hidden;" />'
+      else
+        elem = '<input id="1-' +  n.to_s + '" type="text" class="col-xl-3" />'
+      end
+      html_text += elem
+    end
+    return html_text
+  end
 end
