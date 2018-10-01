22bc47533af0ae991347956818b47e7f0a88fa7b
tanacchi <q111026d@mail.kyutech.jp>
2018-10-01 17:48:32 +0900

    Reject notice when subscribe_user is same with current_user

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -2,6 +2,9 @@
 module IdeasHelper
   def spawn_new_idea_notice(topic)
     subscribe_user = topic.user
+    if subscribe_user == current_user
+      return
+    end
     topic_title_string = topic.title.length > 10 ? topic.title[0..9] + '...' : topic.title
     subscribe_user.notices.create(message: "#{current_user.name}さんがあなたのニーズ「#{topic_title_string}」にアイデアを投稿しました")
   end
