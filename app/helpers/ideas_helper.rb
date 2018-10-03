00ae996513759f05092eb3579c3bb9347b81edd2
tanacchi <q111026d@mail.kyutech.jp>
2018-10-03 14:11:31 +0900

    Add newline to message of notice for view

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -6,6 +6,6 @@ module IdeasHelper
       return
     end
     topic_title_string = topic.title.length > 10 ? topic.title[0..9] + '...' : topic.title
-    subscribe_user.notices.create(message: "#{current_user.name}さんがあなたのニーズ「#{topic_title_string}」にアイデアを投稿しました")
+    subscribe_user.notices.create(message: "#{current_user.name}さんがあなたのニーズ「#{topic_title_string}」に\nアイデアを投稿しました")
   end
 end
