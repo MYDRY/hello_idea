6953bcd670f93bf30ff609c52e80c53666dbe94f
MasatoshiOsugi <q111004m@mail.kyutech.jp>
2018-10-05 06:38:01 +0000

    Add notice into hanburger

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_133848) do
+ActiveRecord::Schema.define(version: 2018_09_08_085954) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
