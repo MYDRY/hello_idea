f7299c7c98648036933cc9e938a5221b076939e2
tanacchi <q111026d@mail.kyutech.jp>
2018-10-16 16:03:16 +0900

    Add test script for sample completion

---
+++ scripts/suggestion.rb
@@ -0,0 +1,32 @@
+# coding: utf-8
+require 'rexml/document'
+require "json"
+require "rubygems"
+require "faraday"
+require "faraday_middleware"
+
+conn = Faraday::Connection.new(:url => 'https://www.google.com') do |builder|
+  builder.request :url_encoded
+  builder.response :logger
+  builder.response :json, :content_type => /\bjson/
+  builder.adapter Faraday.default_adapter
+end
+
+keyword = "東京"
+
+response = conn.get do |req|
+  req.url '/complete/search'
+  req.params['hl'] = "en"
+  req.params['q']  = keyword
+  req.params['output'] = "toolbar"
+  req.params['json'] = true
+end
+
+puts "============================================================"
+puts "keyword: " + keyword
+suggestion_xml = REXML::Document.new(response.body)
+suggestion_xml.elements['toplevel'].each do |elem|
+  p elem.elements['suggestion']['data'].tr!(keyword, '')
+end  
+puts "============================================================"
+
