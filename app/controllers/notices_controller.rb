52a1d60515c231ad50e2745ad05e29d9f71c1412
rkdora <rryuto5210@gmail.com>
2018-10-30 23:56:57 +0900

    Add all mark_as_read

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -10,6 +10,15 @@ class NoticesController < ApplicationController
     redirect_to  user_notices_path(user_id: current_user)
   end
 
+  def mark_all
+    @notices = current_user.notices
+    @notices.each do |notice|
+      notice.mark_as_read
+      notice.save
+    end 
+    redirect_to  user_notices_path(user_id: current_user)
+  end
+
   def destroy
     @notice = Notice.find(params[:id])
     @notice.destroy
