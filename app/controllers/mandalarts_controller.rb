9b1301ca60fae1999406cf47298c4be1b8221604
MO <q111004m@mail.kyutech.jp>
2018-11-03 07:02:56 +0000

    Generate mandal model

--- app/controllers/mandalarts_controller.rb
+++ app/controllers/mandalarts_controller.rb
@@ -1,20 +1,20 @@
 class MandalartsController < ApplicationController
   def index
-      @simple_mandal = SimpleMandal.find_by(user_id: current_user.id)
-      if @simple_mandal.nil?
-          @simple_mandal = ''
-      end
+    @simple_mandal = SimpleMandal.find_by(user_id: current_user.id)
+    if @simple_mandal.nil?
+      @simple_mandal = ''
+    end
   end
   
   def show
-      @simple_mandal = SimpleMandal.find_by(user_id: current_user.id)
-      if @simple_mandal.nil?
-          @simple_mandal = ''
-      end
+    @simple_mandal = SimpleMandal.find_by(user_id: current_user.id)
+    if @simple_mandal.nil?
+      @simple_mandal = ''
+    end
   end
   
   def create
-      @simple_mandal = SimpleMandal.new
-      @simple_mandal.save
+    @simple_mandal = SimpleMandal.new
+    @simple_mandal.save
   end
 end
