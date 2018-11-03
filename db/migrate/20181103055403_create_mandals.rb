c9f20afbe35301bd8f5898519801e5c92918470c
MO <q111004m@mail.kyutech.jp>
2018-11-03 07:02:56 +0000

    Generate mandal model

---
+++ db/migrate/20181103055403_create_mandals.rb
@@ -0,0 +1,17 @@
+class CreateMandals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :mandals do |t|
+      t.string :core
+      t.string :s1
+      t.string :s2
+      t.string :s3
+      t.string :s4
+      t.string :s5
+      t.string :s6
+      t.string :s7
+      t.string :s8
+
+      t.timestamps
+    end
+  end
+end
