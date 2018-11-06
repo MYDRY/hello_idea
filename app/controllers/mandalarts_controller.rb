5640a1f70d9ec91f2b25f638d13600b782d60886
MO <q111004m@mail.kyutech.jp>
2018-11-06 10:00:14 +0000

    Add show in controller

--- app/controllers/mandalarts_controller.rb
+++ app/controllers/mandalarts_controller.rb
@@ -1,5 +1,18 @@
 class MandalartsController < ApplicationController
+  def show
+    @simple_mandal = SimpleMandal.find(params[:id])
+    if @simple_mandal.nil?
+      @simple_mandal = ''
+    end
+  end
+  
+  def create
+    @simple_mandal = SimpleMandal.new(params[])
+    @simple_mandal.user_id = current_user
+    @simple_mandal.save
+  end
+  
   def index
-      @mandals = Mandal.all
+      
   end
 end
