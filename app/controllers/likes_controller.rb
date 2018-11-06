ec6d602dab4146c70916abbc1bdd6b4f36079e11
rkdora <rryuto5210@gmail.com>
2018-11-06 17:21:05 +0900

    Add js like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,22 @@
 # coding: utf-8
 class LikesController < ApplicationController
   before_action :authorize
-  
+
   def create
     @idea = Idea.find(params[:idea_id])
     like = current_user.likes.build(idea_id: @idea.id)
     like.save
     view_context.spawn_like_notice(@idea)
     flash[:success] = "いいねしました"
-    redirect_back fallback_location: topic_path(id: @idea.topic)
+  #  redirect_back fallback_location: topic_path(id: @idea.topic)
+    @idea.reload
   end
 
   def destroy
     @idea = Idea.find(params[:id])
     like = Like.find_by(idea_id: @idea.id, user_id: current_user.id)
-    like.destroy 
+    like.destroy
     flash[:success] = "いいねを取り消しました"
-    redirect_back fallback_location: topic_path(id: @idea.topic)
+    @idea.reload
   end
 end
