d2241b749442d433001c8d97c95364d5e5184d2b
Ryuto Kawano <rryuto5210@gmail.com>
2018-11-13 17:01:45 +0900

    Add model Word

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_08_085954) do
+ActiveRecord::Schema.define(version: 2018_11_13_074726) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -74,6 +74,12 @@ ActiveRecord::Schema.define(version: 2018_09_08_085954) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
