7be7a5c4981b60c9c363f537f876f33e9698a7f6
rkdora <rryuto5210@gmail.com>
2018-11-13 17:36:44 +0900

    Add view words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,24 @@
 class WordsController < ApplicationController
   def index
+    @words = Word.all
+  end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_params)
+    if @word.save
+      redirect_to words_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def word_params
+    params.require(:word).permit(:title)
   end
 end
