744e5e764537b8015a6924e43191b9344ec0d8b0
ryuto <rryuto5210@gmail.com>
2018-12-25 21:03:04 +0900

    Add index randomwordsidea

--- app/controllers/random_words_ideas_controller.rb
+++ app/controllers/random_words_ideas_controller.rb
@@ -1,4 +1,13 @@
 class RandomWordsIdeasController < ApplicationController
+  def index
+    word1 = params[:word1]
+    word2 = params[:word2]
+    @ideas = RandomWordsIdea.where(word1_id: [word1, word2])
+                            .where(word2_id: [word1, word2])
+    @word1 = Word.find(word1)
+    @word2 = Word.find(word2)
+  end
+
   def create
     @sea = Sea.new(sea_params)
     @sea.user_id = current_user.id
@@ -11,7 +20,7 @@ class RandomWordsIdeasController < ApplicationController
     @random.word2_id = params[:sea][:word2]
     @random.sea_id = @sea.id
     if @random.save
-      redirect_to words_path
+      redirect_to random_words_ideas_path(word1: @random.word1_id, word2: @random.word2_id)
     else
       redirect_to root_path #仮
     end
