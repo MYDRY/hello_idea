50ec522d45b8476a856e5aaa1e6f9f945b7061aa
ryuto <rryuto5210@gmail.com>
2018-12-25 22:30:16 +0900

    Add randomwords in sea show

--- app/controllers/seas_controller.rb
+++ app/controllers/seas_controller.rb
@@ -3,5 +3,27 @@
 class SeasController < ApplicationController
   def index
     @seas = Sea.all
+    @sea = Sea.new
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+    if (@random_words = RandomWordsIdea.left_joins(:sea).where(sea_id: @sea.id).first)
+    end
+  end
+
+  def create
+    @sea = current_user.seas.build(sea_params)
+    if @sea.save
+      redirect_to @sea
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def sea_params
+    params.require(:sea).permit(:body)
   end
 end
