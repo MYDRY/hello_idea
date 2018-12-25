f135bb08280626f3c3d41dd00c55c59a257de1fb
tanacchi <q111026d@mail.kyutech.jp>
2018-12-25 18:19:03 +0900

    Skip creating invest when amount is zero

--- app/controllers/invests_controller.rb
+++ app/controllers/invests_controller.rb
@@ -2,6 +2,7 @@ class InvestsController < ApplicationController
   def create
     invested_topic = Topic.find(params[:topic_id])
     invests_amount = params[:invests_amount].to_i
+    redirect_to invested_topic if invests_amount == 0
     invest = current_user.invests.build(topic_id: invested_topic.id, amount: invests_amount)
     invest.save
     invested_topic.get_supported(invests_amount)
