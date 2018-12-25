9d6045bcab0e20552727fda4a1388013d2700115
tanacchi <q111026d@mail.kyutech.jp>
2018-12-25 17:42:17 +0900

    Upgrade invests#create method for new invest

--- app/controllers/invests_controller.rb
+++ app/controllers/invests_controller.rb
@@ -1,5 +1,12 @@
 class InvestsController < ApplicationController
   def create
+    invested_topic = Topic.find(params[:topic_id])
+    invests_amount = params[:invests_amount].to_i
+    invest = current_user.invests.build(topic_id: invested_topic.id, amount: invests_amount)
+    invest.save
+    invested_topic.get_supported(invests_amount)
+    current_user.change_point(-invests_amount)
+    redirect_to invested_topic
   end
 
   def edit
