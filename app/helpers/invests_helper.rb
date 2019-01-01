d9e91991f6dd59d38e6e551028898c016da80517
tanacchi <q111026d@mail.kyutech.jp>
2019-01-01 19:15:21 +0900

    Add invests-helper method named calc_invest_rates

--- app/helpers/invests_helper.rb
+++ app/helpers/invests_helper.rb
@@ -1,2 +1,12 @@
 module InvestsHelper
+  def calc_invest_rates(topic)
+    total = topic.support
+    invests = Invest.where(topic_id: topic.id)
+    results = []
+    invests.each do |invest|
+      results << { user_id: invest.user_id, 
+                   rate: invest.amount / total.to_f }
+    end
+    results
+  end
 end
