c5e5a193cd118b8cc756d28a743a5f3eeddbc3ca
tanacchi <q111026d@mail.kyutech.jp>
2019-01-01 19:15:21 +0900

    Add invests-helper method named calc_invest_rates

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,6 +2,7 @@
 
 class IdeasController < ApplicationController
   include LikesHelper
+  include InvestsHelper
 
   before_action :authorize, only: %i[new create]
   before_action :ensure_correct_user, only: %i[edit update destroy]
@@ -27,6 +28,7 @@ class IdeasController < ApplicationController
       unless topic.user == @idea.user
         topic.user.change_point(20)
         @idea.user.change_point(10)
+        p view_context.calc_invest_rates(topic)
       end
       flash[:success] = 'アイデアを投稿しました'
       view_context.spawn_new_idea_notice(topic)
