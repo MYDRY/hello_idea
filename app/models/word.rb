9a8c01f292f0d2f45ddb736ad72a2b268b5a5319
ryuto <rryuto5210@gmail.com>
2019-01-10 16:23:12 +0900

    Add dependent: :destroy

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,6 +3,6 @@
 class Word < ApplicationRecord
   validates :word, presence: true, uniqueness: true
 
-  has_many :random_words_ideas_of_word1, class_name: 'RandomWordsIdea', foreign_key: 'word1_id'
-  has_many :random_words_ideas_of_word2, class_name: 'RandomWordsIdea', foreign_key: 'word2_id'
+  has_many :random_words_ideas_of_word1, dependent: :destroy, class_name: 'RandomWordsIdea', foreign_key: 'word1_id'
+  has_many :random_words_ideas_of_word2, dependent: :destroy, class_name: 'RandomWordsIdea', foreign_key: 'word2_id'
 end
